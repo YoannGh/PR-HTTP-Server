e957e8c76bf816c9304606fe18038f4302179256
y0koz <yoann.ghigoff@gmail.com>
2016-10-06 12:46:14 +0200

    started TD3

--- 3./TD3/src/chaine_proc.c
+++ 3./TD3/src/chaine_proc.c
@@ -1,12 +1,58 @@
 #define _XOPEN_SOURCE 700
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <time.h>
 
 int main(int argc, char const *argv[])
 {
-	/* code */
-	return 0;
+	int* pids;
+	int nbProc, i, p, random;
+
+	if (argc != 2)
+	{
+		printf("Error args\n");
+		return EXIT_FAILURE;
+	}
+
+	nbProc = atoi(argv[1]);
+
+	pids = malloc(nbProc+1);
+
+	for(i = 0; i < nbProc+1; i++) {
+		printf("%d\n", i);
+		pids[i] = getpid();
+		if((p = fork()) == -1) {
+ 			perror("Erreur fork");
+ 		}
+ 		else if(p != 0) {
+ 			if(i == 0)
+ 				printf("pere ici\n");
+ 			break;
+ 		}
+	}
+
+	if((i < nbProc+1) && (i != 0)) {
+		printf("%d Termine: pid: %d, ppid: %d, fpid: %d\n", i, getpid(), getppid(), wait(&random));
+		return random;
+	}
+	else if(i == 0) {
+		printf("%d Termine: pid: %d, ppid: %d, fpid: %d\n", i, getpid(), getppid(), wait(&random));
+		printf("Main proc: random: %d\n", random);
+		return EXIT_SUCCESS;
+	}
+
+	printf("Dernier fils: pids:\n");
+	for(i = 0; i < nbProc+1; i++)
+		printf("%d ", pids[i]);
+	printf("\n");
+
+
+	srand(time(NULL));
+	random = (int)(rand () /(((double) RAND_MAX +1) /100));
+	printf("random dernier fils: %d\n", random);
+	return random;
 }
