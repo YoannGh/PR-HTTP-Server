be586373e57714837e2db3aa06b755f286979cbe
y0koz <y0koz@users.noreply.github.com>
2016-12-26 18:22:11 +0100

    finished logger

--- projet/include/logger.h
+++ projet/include/logger.h
@@ -1,4 +1,7 @@
 #include <pthread.h>
+#include <time.h>
+#include <netinet/in.h>
+#include <sys/types.h>
 
 #define LOG_PATH "/tmp/http3506454.log"
 
@@ -11,4 +14,4 @@ int logger_init(logger* log);
 
 int logger_destroy(logger* log);
 
-void log(logger* log, struct in_addr caddress, time_t time, pid_t spid, pthread_t stid, char* req_firstline, int retcode, int filesize);
+void log_request(logger* log, struct in_addr caddress, time_t time, pid_t spid, pthread_t stid, char* request, int retcode, double filesize);
